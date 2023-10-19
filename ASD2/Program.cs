2f2b1cb0838fa4a412edde5269f5ae2b8de21b67
Werykowiak <werykowiak@gmail.com>
2023-10-19 23:43:06 +0200

    Czytanie pliku

--- ASD2/Program.cs
+++ ASD2/Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StreamReader streamReader = new StreamReader("input.txt");
+            var fileLine = streamReader.ReadLine();
+
+            string[] fLine = fileLine.Split(" ");
+            int n = int.Parse(fLine[0]);
+            int m = int.Parse(fLine[0]);
+            int d = int.Parse(fLine[0]);
+
+            List<Layer> cave = new List<Layer>();
+
+            for (int i = 0; i < d; i++)
+            {
+                Layer layer = new Layer();
+                for (int j = 0; j < m; j++)
+                {
+                    fileLine = streamReader.ReadLine();
+                    Line line = new Line();
+                    line.blocks = fileLine.ToCharArray().ToList();
+                    layer.lines.Add(line);
+                }
+                cave.Add(layer);
+            }
+            streamReader.Close();
+
+            foreach (Layer layer in cave)
+            {
+                foreach(Line line in layer.lines)
+                {
+                    foreach(char block in line.blocks)
+                    {
+                        Console.Write(block);
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
         }
+    }
+
+    public class Layer
+    {
+        public List<Line> lines;
+        public Layer() { lines = new List<Line>(); }
+    }
+    public class Line
+    {
+        public List<char> blocks;
+        public Line() { blocks = new List<char>(); }
     }
 }
