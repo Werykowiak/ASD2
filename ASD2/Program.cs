156d76798347786debc9f695cacf5ebd53bda121
Werykowiak <werykowiak@gmail.com>
2023-10-28 19:13:08 +0200

    3/3 job is done. War is over

--- ASD2/Program.cs
+++ ASD2/Program.cs
@@ -1,20 +1,24 @@
-﻿namespace ASD2
+﻿using System.Diagnostics;
+namespace ASD2
 {
     internal class Program
     {
         static int n;
         static int m;
         static int d;
+
         static int count = 0;
         static List<Layer> cave;
-        static Dictionary<string, bool> prawoMarcina;
-        static extern bool SetThreadStackGuarantee(ref uint StackSizeInBytes);
+        static Dictionary<string, bool> visitedCaves;
+        static bool isSurface;
+        //static extern bool SetThreadStackGuarantee(ref uint StackSizeInBytes);
         static void Main(string[] args)
         {
-            StreamReader streamReader = new StreamReader("in1.txt");
+
+            StreamReader streamReader = new StreamReader("maly2.txt");
             var fileLine = streamReader.ReadLine();
-            
-            uint stackSizeInBytes = 16 * 1024 * 1024;
+
+            //uint stackSizeInBytes = 16 * 1024 * 1024;
             string[] fLine = fileLine.Split(' ');
             n = int.Parse(fLine[0]);
             m = int.Parse(fLine[1]);
@@ -37,137 +41,72 @@
             }
             streamReader.Close();
             Console.WriteLine($"{n}, {m}, {d}");
-            /*foreach (Layer layer in cave)
-            {
-                foreach (Line line in layer.lines)
-                {
-                    foreach (char block in line.blocks)
-                    {
-                        Console.Write(block);
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine();
-            }*/
-            //Console.WriteLine(cave[3].lines[4].blocks[4]);
-            //Console.WriteLine(chief());
-            baxton();
+
+            Thread thread = new Thread(caveAnalysis, 8 * 1024 * 1024);
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            thread.Start();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
         }
-        /*static int chief()
+
+        static void caveAnalysis()
         {
-            int temp = 0;
-            for (int t = 0; t < m; t++)
-            {
-                for (int x = 0; x < n; x++)
-                {
-                    if (cave[0].lines[t].blocks[x] == 'o')
-                    {
-                        prawoMarcina = new Dictionary<string, bool>();
-                        int temp2 = glebiej(1, t, x);
-                        if (temp2 > temp) { temp = temp2; }
-                    }
-                }
-            }
-            //temp = glebiej(1, 3, 2);
-            return temp + 1;
-        }*/
-        static void baxton()
-        {
-            prawoMarcina = new Dictionary<string, bool>();
-            long obj = 0;
+            int isolated = 0;
+            int maxDepth = 0;
+            visitedCaves = new Dictionary<string, bool>();
+            long volume = 0;
             for (int a = d - 1; a > 0; a--)
             {
                 for (int t = 0; t < m; t++)
                 {
                     for (int x = 0; x < n; x++)
                     {
-                        if (cave[a].lines[t].blocks[x] == 'o' && !prawoMarcina.ContainsKey($"{a},{t},{x}"))
+                        if (cave[a].lines[t].blocks[x] == 'o' && !visitedCaves.ContainsKey($"{a},{t},{x}"))
                         {
-                            //Console.WriteLine($"{a},{x},{t}");
+                            isSurface = false;
                             long temp = Up(a, t, x);
-                            if (temp > obj) { obj = temp; }
+                            if (temp > volume) { volume = temp; }
+                            if (!isSurface) { isolated++; } else if (a > maxDepth) { maxDepth = a; }
                         }
                     }
                 }
             }
 
-            Console.WriteLine($"Największa objętość: {obj} ");
+            Console.WriteLine($"Największa objętość: {volume}, Jaskinie izolowane: {isolated}, Największa głębokość: {maxDepth + 1}");
         }
 
-        /*static int glebiej(int i, int j, int k)
-        {
-            prawoMarcina.Add($"{i},{j},{k}", true);
-            int temp = 1;
-            if (temp == d - 1) { return temp; }
-            //Console.WriteLine($"Warstwa: {i}, X: {k}, Y: {j}, temp: {temp} , {cave[i].lines[j].blocks[k]}");
-            if (i < d - 1 && !prawoMarcina.ContainsKey($"{i + 1},{j},{k}") && cave[i + 1].lines[j].blocks[k] == 'o')
-            {
-                temp = glebiej(i + 1, j, k);
-            }
-            if (i > 0 && !prawoMarcina.ContainsKey($"{i - 1},{j},{k}") && cave[i - 1].lines[j].blocks[k] == 'o')
-            {
-                temp += Up(i - 1, j, k);
-            }
-            if (j < m - 1 && !prawoMarcina.ContainsKey($"{i},{j + 1},{k}") && cave[i].lines[j + 1].blocks[k] == 'o')
-            {
-                temp += glebiej(i, j + 1, k);
-                //if (temp2 > temp) { temp = temp2; }
-            }
-
-            if (j > 0 && !prawoMarcina.ContainsKey($"{i},{j - 1},{k}") && cave[i].lines[j - 1].blocks[k] == 'o')
-            {
-                temp += glebiej(i, j - 1, k);
-                //if (temp2 > temp) { temp = temp2; }
-            }
-
-            if (k < n - 1 && !prawoMarcina.ContainsKey($"{i},{j},{k + 1}") && cave[i].lines[j].blocks[k + 1] == 'o')
-            {
-                temp += glebiej(i, j, k + 1);
-                //if (temp2 > temp) { temp = temp2; }
-            }
-
-            if (k > 0 && !prawoMarcina.ContainsKey($"{i},{j},{k - 1}") && cave[i].lines[j].blocks[k - 1] == 'o')
-            {
-                temp += glebiej(i, j, k - 1);
-                //if (temp2 > temp) { temp = temp2; }
-            }
-            return temp;
-        }*/
         static long Up(int i, int j, int k)
         {
-            prawoMarcina.Add($"{i},{j},{k}", true);
+            visitedCaves.Add($"{i},{j},{k}", true);
             long temp = 1;
-            //Console.WriteLine($"Warstwa: {i}, X: {k}, Y: {j}, temp: {temp} , {cave[i].lines[j].blocks[k]}");
-            if (i > 0 && !prawoMarcina.ContainsKey($"{i - 1},{j},{k}") && cave[i - 1].lines[j].blocks[k] == 'o')
+            if (i > 0 && cave[i - 1].lines[j].blocks[k] == 'o' && !visitedCaves.ContainsKey($"{i - 1},{j},{k}"))
             {
                 temp += Up(i - 1, j, k);
             }
-            if (i < d - 1 && !prawoMarcina.ContainsKey($"{i + 1},{j},{k}") && cave[i + 1].lines[j].blocks[k] == 'o')
+            else if (i == 0) { isSurface = true; }
+            if (i < d - 1 && cave[i + 1].lines[j].blocks[k] == 'o' && !visitedCaves.ContainsKey($"{i + 1},{j},{k}"))
             {
                 temp += Up(i + 1, j, k);
             }
-            if (j < m - 1 && !prawoMarcina.ContainsKey($"{i},{j + 1},{k}") && cave[i].lines[j + 1].blocks[k] == 'o')
+            if (j < m - 1 && cave[i].lines[j + 1].blocks[k] == 'o' && !visitedCaves.ContainsKey($"{i},{j + 1},{k}"))
             {
                 temp += Up(i, j + 1, k);
-                //if (temp2 > temp) { temp = temp2; }
             }
 
-            if (j > 0 && !prawoMarcina.ContainsKey($"{i},{j - 1},{k}") && cave[i].lines[j - 1].blocks[k] == 'o')
+            if (j > 0 && cave[i].lines[j - 1].blocks[k] == 'o' && !visitedCaves.ContainsKey($"{i},{j - 1},{k}"))
             {
                 temp += Up(i, j - 1, k);
-                //if (temp2 > temp) { temp = temp2; }
             }
 
-            if (k < n - 1 && !prawoMarcina.ContainsKey($"{i},{j},{k + 1}") && cave[i].lines[j].blocks[k + 1] == 'o')
+            if (k < n - 1 && cave[i].lines[j].blocks[k + 1] == 'o' && !visitedCaves.ContainsKey($"{i},{j},{k + 1}"))
             {
                 temp += Up(i, j, k + 1);
-                //if (temp2 > temp) { temp = temp2; }
             }
 
-            if (k > 0 && !prawoMarcina.ContainsKey($"{i},{j},{k - 1}") && cave[i].lines[j].blocks[k - 1] == 'o')
+            if (k > 0 && cave[i].lines[j].blocks[k - 1] == 'o' && !visitedCaves.ContainsKey($"{i},{j},{k - 1}"))
             {
                 temp += Up(i, j, k - 1);
-                //if (temp2 > temp) { temp = temp2; }
             }
             return temp;
         }
